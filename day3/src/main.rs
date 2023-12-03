c6905ad7120286571554d6e02d7b204d163c57ff
alexwang0311 <zhixiang.wang@vanderbilt.edu>
2023-12-03 01:11:56 -0600

    day3 p2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -17,10 +17,23 @@ fn main() {
         //println!("Element at position {}: {:?}", row, line);
         for col in 0..schema[0].len() {
             let c: char = schema[row][col];
+            let snapshot = schema.clone();
+            let mut numbers: Vec<String> = Vec::new();
             if !c.is_alphanumeric() && c != '.' {
                 let x: i32 = row.try_into().unwrap();
                 let y: i32 = col.try_into().unwrap();
-                println!("special character at {},{}", x, y);
+                //println!("special character at {},{}", x, y);
+                //p2
+                search_p2(x - 1, y - 1, &mut schema, &mut numbers);
+                search_p2(x - 1, y, &mut schema, &mut numbers);
+                search_p2(x - 1, y + 1, &mut schema, &mut numbers);
+                search_p2(x, y - 1, &mut schema, &mut numbers);
+                search_p2(x, y + 1, &mut schema, &mut numbers);
+                search_p2(x + 1, y - 1, &mut schema, &mut numbers);
+                search_p2(x + 1, y, &mut schema, &mut numbers);
+                search_p2(x + 1, y + 1, &mut schema, &mut numbers);
+                //p1
+                /*
                 search(x - 1, y - 1, &mut schema, &mut sum);
                 search(x - 1, y, &mut schema, &mut sum);
                 search(x - 1, y + 1, &mut schema, &mut sum);
@@ -29,7 +42,16 @@ fn main() {
                 search(x + 1, y - 1, &mut schema, &mut sum);
                 search(x + 1, y, &mut schema, &mut sum);
                 search(x + 1, y + 1, &mut schema, &mut sum);
+                */
             }
+            //p2
+            if numbers.len() == 2 {
+                println!("found gear {}, {}", numbers[0], numbers[1]);
+                sum += numbers[0].parse::<i32>().unwrap() * numbers[1].parse::<i32>().unwrap();
+            }
+            //p2
+            //NOTE: need to store the schema state after each search
+            schema = snapshot;
         } 
     }
     println!("{}", sum);
@@ -53,6 +75,37 @@ fn find_number(x: i32, y:i32, schema: &mut Vec<Vec<char>>, sum: &mut i32) {
     }
     //println!("{}", number);
     *sum += number.parse::<i32>().unwrap();
+}
+
+fn find_number_p2(x: i32, y:i32, schema: &mut Vec<Vec<char>>, numbers: &mut Vec<String>) {
+    let mut number: String = schema[x as usize][y as usize].to_string();
+    //println!("number found at {},{}: {}", x, y, number);
+    //search left
+    let mut left: i32 = y - 1;
+    while left >= 0 && schema[x as usize][left as usize].is_digit(10) {
+        number = schema[x as usize][left as usize].to_string() + &number;
+        schema[x as usize][left as usize] = '.';
+        left -= 1;
+    }
+    let mut right: i32 = y + 1;
+    while right < schema[0].len().try_into().unwrap() && schema[x as usize][right as usize].is_digit(10) {
+        number = number + &schema[x as usize][right as usize].to_string();
+        schema[x as usize][right as usize] = '.';
+        right += 1;
+    }
+    //println!("{}", number);
+    numbers.push(number);
+}
+
+fn search_p2(x: i32, y: i32, schema: &mut Vec<Vec<char>>, numbers: &mut Vec<String>){
+    let x_max: usize = schema.len();
+    let y_max: usize = schema[0].len();
+    if x >= 0 && y >= 0 && x < x_max.try_into().unwrap() && y < y_max.try_into().unwrap() {
+        let c: char = schema[x as usize][y as usize];
+        if c.is_digit(10) {
+            find_number_p2(x, y, schema, numbers);
+        }
+    }
 }
 
 fn search(x: i32, y: i32, schema: &mut Vec<Vec<char>>, sum: &mut i32) {
