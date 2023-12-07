8624da88a337f6ede2ce531ab9fcf7464d7dd77d
alexwang0311 <zhixiang.wang@vanderbilt.edu>
2023-12-07 11:28:58 -0600

    fixed bad code with idiomatic rust -> pattern matching and functional vec operations

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,12 +2,27 @@ use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::{cmp, char};
+use std::char;
 use std::collections::HashMap;
+use std::time::Instant;
+
+#[derive(Eq, PartialEq, PartialOrd, Ord, Debug)]
+enum Type {
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard
+}
 
 #[derive(Eq, PartialEq, Debug)]
 struct Hand{
-    cards: Vec<u32>
+    cards: Vec<u32>,
+    hand_type: Type,
+    bid: u32,
+    str: String
 }
 
 impl PartialOrd for Hand {
@@ -18,117 +33,51 @@ impl PartialOrd for Hand {
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        match (self.cards[0].cmp(&other.cards[0]), self.cards[1].cmp(&other.cards[1]), self.cards[2].cmp(&other.cards[2]), self.cards[3].cmp(&other.cards[3]), self.cards[4].cmp(&other.cards[4])) {
-            (Ordering::Equal, Ordering::Equal, Ordering::Equal, Ordering::Equal, _) => self.cards[4].cmp(&other.cards[4]),
-            (Ordering::Equal, Ordering::Equal, Ordering::Equal, _, _) => self.cards[3].cmp(&other.cards[3]),
-            (Ordering::Equal, Ordering::Equal, _, _, _) => self.cards[2].cmp(&other.cards[2]),
-            (Ordering::Equal, _, _, _, _) => self.cards[1].cmp(&other.cards[1]),
-            (_, _, _, _, _) => self.cards[0].cmp(&other.cards[0])
+        match other.hand_type.cmp(&self.hand_type) {
+            Ordering::Equal => {
+                match (self.cards[0].cmp(&other.cards[0]), self.cards[1].cmp(&other.cards[1]), self.cards[2].cmp(&other.cards[2]), self.cards[3].cmp(&other.cards[3]), self.cards[4].cmp(&other.cards[4])) {
+                    (Ordering::Equal, Ordering::Equal, Ordering::Equal, Ordering::Equal, _) => self.cards[4].cmp(&other.cards[4]),
+                    (Ordering::Equal, Ordering::Equal, Ordering::Equal, _, _) => self.cards[3].cmp(&other.cards[3]),
+                    (Ordering::Equal, Ordering::Equal, _, _, _) => self.cards[2].cmp(&other.cards[2]),
+                    (Ordering::Equal, _, _, _, _) => self.cards[1].cmp(&other.cards[1]),
+                    (_, _, _, _, _) => self.cards[0].cmp(&other.cards[0])
+                }
+            },
+            _ => other.hand_type.cmp(&self.hand_type)
         }
-    }
-}
-
-#[derive(Eq, PartialEq, Debug)]
-enum Type {
-    FiveOfAKind(Hand),
-    FourOfAKind(Hand),
-    FullHouse(Hand),
-    ThreeOfAKind(Hand),
-    TwoPair(Hand),
-    OnePair(Hand),
-    HighCard(Hand)
-}
-
-impl PartialOrd for Type {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Type {
-    fn cmp(&self, other: &Self) -> cmp::Ordering {
-        match (self, other) {
-            (Type::FiveOfAKind(val), Type::FiveOfAKind(other_val)) => val.cmp(other_val),
-            (Type::FiveOfAKind(_), _) => Ordering::Greater,
-            (Type::FourOfAKind(_), Type::FiveOfAKind(_)) => Ordering::Less,
-            (Type::FourOfAKind(val), Type::FourOfAKind(other_val)) => val.cmp(other_val),
-            (Type::FourOfAKind(_), _) => Ordering::Greater,
-            (Type::FullHouse(_), Type::FiveOfAKind(_)) => Ordering::Less,
-            (Type::FullHouse(_), Type::FourOfAKind(_)) => Ordering::Less,
-            (Type::FullHouse(val), Type::FullHouse(other_val)) => val.cmp(other_val),
-            (Type::FullHouse(_), _) => Ordering::Greater,
-            (Type::ThreeOfAKind(_), Type::FiveOfAKind(_)) => Ordering::Less,
-            (Type::ThreeOfAKind(_), Type::FourOfAKind(_)) => Ordering::Less,
-            (Type::ThreeOfAKind(_), Type::FullHouse(_)) => Ordering::Less,
-            (Type::ThreeOfAKind(val), Type::ThreeOfAKind(other_val)) => val.cmp(other_val),
-            (Type::ThreeOfAKind(_), _) => Ordering::Greater,
-            (Type::TwoPair(_), Type::FiveOfAKind(_)) => Ordering::Less,
-            (Type::TwoPair(_), Type::FourOfAKind(_)) => Ordering::Less,
-            (Type::TwoPair(_), Type::FullHouse(_)) => Ordering::Less,
-            (Type::TwoPair(_), Type::ThreeOfAKind(_)) => Ordering::Less,
-            (Type::TwoPair(val), Type::TwoPair(other_val)) => val.cmp(other_val),
-            (Type::TwoPair(_), _) => Ordering::Greater,
-            (Type::OnePair(_), Type::FiveOfAKind(_)) => Ordering::Less,
-            (Type::OnePair(_), Type::FourOfAKind(_)) => Ordering::Less,
-            (Type::OnePair(_), Type::FullHouse(_)) => Ordering::Less,
-            (Type::OnePair(_), Type::ThreeOfAKind(_)) => Ordering::Less,
-            (Type::OnePair(_), Type::TwoPair(_)) => Ordering::Less,
-            (Type::OnePair(val), Type::OnePair(other_val)) => val.cmp(other_val),
-            (Type::OnePair(_), _) => Ordering::Greater,
-            (Type::HighCard(val), Type::HighCard(other_val)) => val.cmp(other_val),
-            (Type::HighCard(_), _) => Ordering::Less
-        }
-    }
-}
-
-#[derive(Debug, Eq, PartialEq)]
-struct Entry {
-    hand_type: Type,
-    bid: u32,
-    cards: String
-}
-
-impl PartialOrd for Entry {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Entry {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.hand_type.cmp(&other.hand_type)
     }
 }
 
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-        let mut entries: Vec<Entry> = Vec::new();
+        let clock = Instant::now();
+
+        let mut entries: Vec<Hand> = Vec::new();
         for line in lines {
             if let Ok(text) = line {
                 if let Some((cards, bid)) = text.split_once(" ") {
-                    //println!("{}, {}", cards, bid);
                     //p2
                     if let Some(hand) = build_hand_p2(cards, bid.parse::<u32>().unwrap()) {
                     //p1
                     //if let Some(hand) = build_hand(cards, bid.parse::<u32>().unwrap()) {
-                        //println!("{:?}", hand);
+                        //println!("cards: {}, bid: {} | hand: {:?}", cards, bid, hand);
                         entries.push(hand);
+                    }
+                    else {
+                        panic!("failed to parse hand: {} | {}", cards, bid);
                     }
                 }
             }
         }
         entries.sort();
-        //let entries = entries.iter().map(|e| &e.cards).collect::<Vec<&String>>();
-        //println!("{:?}", entries);
-        let bids = entries.iter().map(|e| usize::try_from(e.bid).unwrap()).collect::<Vec<usize>>();
-        //println!("{:?}", bids);
+        //println!("cards: {:?}", entries.iter().map(|e| &e.str).collect::<Vec<&String>>());
+        //println!("bids: {:?}", entries.iter().map(|e| usize::try_from(e.bid).unwrap()).collect::<Vec<usize>>());
         
-        let mut winning: usize = 0;
-        for (index , bid) in bids.iter().enumerate() {
-            winning += *bid * (index + 1);
-        }
-        println!("{}", winning);
-        
+        let winning = entries.iter()
+                                    .map(|e| usize::try_from(e.bid).unwrap())
+                                    .enumerate()
+                                    .fold(0, |sum, (index, bid)| sum + (index + 1) * bid);
+        println!("ans: {} | time {:?}", winning, clock.elapsed());
     }
 }
 
@@ -147,7 +96,7 @@ fn char_to_u32_p2(char: &char) -> u32 {
     }
 }
 
-fn build_hand_p2(str: &str, bid: u32) -> Option<Entry> {
+fn build_hand_p2(str: &str, bid: u32) -> Option<Hand> {
     let mut hm: HashMap<char, u32> = HashMap::new();
     let mut j_count: u32 = 0;
     for char in str.chars() {
@@ -161,43 +110,43 @@ fn build_hand_p2(str: &str, bid: u32) -> Option<Entry> {
     }
 
     let cards = str.chars().into_iter().map(|c| char_to_u32_p2(&c)).collect::<Vec<u32>>();
-    let hand = Hand {
-        cards: cards
-    };
 
     match hm.keys().len() {
-        5 => {
-            Some(Entry {
-                hand_type: Type::HighCard(hand),
-                bid: bid,
-                cards: str.to_owned()
-            })
-        },
-        4 => Some(Entry {
-            hand_type: Type::OnePair(hand),
+        5 => Some(Hand {
+            hand_type: Type::HighCard,
+            cards: cards,
             bid: bid,
-            cards: str.to_owned()
+            str: str.to_owned()
+        }),
+        4 => Some(Hand {
+            hand_type: Type::OnePair,
+            cards: cards,
+            bid: bid,
+            str: str.to_owned()
         }),
         3 => {
             match j_count {
-                1 | 2 => Some(Entry {
-                    hand_type: Type::ThreeOfAKind(hand),
+                1 | 2 => Some(Hand {
+                    hand_type: Type::ThreeOfAKind,
+                    cards: cards,
                     bid: bid,
-                    cards: str.to_owned()
+                    str: str.to_owned()
                 }),
                 _ => {
                     if let Some(_) = hm.keys().into_iter().filter(|key| hm[key] == 3).collect::<Vec<_>>().get(0) {
-                        Some(Entry {
-                            hand_type: Type::ThreeOfAKind(hand),
+                        Some(Hand {
+                            hand_type: Type::ThreeOfAKind,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                     else {
-                        Some(Entry {
-                            hand_type: Type::TwoPair(hand),
+                        Some(Hand {
+                            hand_type: Type::TwoPair,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                 }
@@ -207,53 +156,61 @@ fn build_hand_p2(str: &str, bid: u32) -> Option<Entry> {
             match j_count {
                 1 => {
                     if let Some(_) = hm.keys().into_iter().filter(|key| hm[key] == 3).collect::<Vec<_>>().get(0) {
-                        Some(Entry {
-                            hand_type: Type::FourOfAKind(hand),
+                        Some(Hand {
+                            hand_type: Type::FourOfAKind,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                     else {
-                        Some(Entry {
-                            hand_type: Type::FullHouse(hand),
+                        Some(Hand {
+                            hand_type: Type::FullHouse,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                 },
-                2 | 3 => Some(Entry {
-                    hand_type: Type::FourOfAKind(hand),
+                2 | 3 => Some(Hand {
+                    hand_type: Type::FourOfAKind,
+                    cards: cards,
                     bid: bid,
-                    cards: str.to_owned()
+                    str: str.to_owned()
                 }),
                 _ => {
                     if let Some(_) = hm.keys().into_iter().filter(|key| hm[key] == 3).collect::<Vec<_>>().get(0) {
-                        Some(Entry {
-                            hand_type: Type::FullHouse(hand),
+                        Some(Hand {
+                            hand_type: Type::FullHouse,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                     else {
-                        Some(Entry {
-                            hand_type: Type::FourOfAKind(hand),
+                        Some(Hand {
+                            hand_type: Type::FourOfAKind,
+                            cards: cards,
                             bid: bid,
-                            cards: str.to_owned()
+                            str: str.to_owned()
                         })
                     }
                 }
             }
         },
-        1 | 0 => Some(Entry {
-            hand_type: Type::FiveOfAKind(hand),
+        //NOTE: spent waaaaaaaay too much time figuring out edge case 'JJJJJ' -> 0 key
+        1 | 0 => Some(Hand {
+            hand_type: Type::FiveOfAKind,
+            cards: cards,
             bid: bid,
-            cards: str.to_owned()
+            str: str.to_owned()
         }),
         _ => None
     }
 }
 
-/* 
+//p1
+/*
 fn char_to_u32(char: &char) -> u32 {
     if char.is_digit(10) {
         return char.to_digit(10).unwrap()
@@ -269,62 +226,74 @@ fn char_to_u32(char: &char) -> u32 {
     }
 }
 
-fn build_hand(str: &str, bid: u32) -> Option<Entry> {
+fn build_hand(str: &str, bid: u32) -> Option<Hand> {
     let mut hm: HashMap<char, u32> = HashMap::new();
     for char in str.chars() {
         let val = hm.entry(char).or_insert(0);
         *val += 1;
     }
     let cards = str.chars().into_iter().map(|c| char_to_u32(&c)).collect::<Vec<u32>>();
-    let hand = Hand {
-        cards: cards
-    };
+
     match hm.keys().len() {
         5 => {
-            Some(Entry {
-                hand_type: Type::HighCard(hand),
-                bid: bid
+            Some(Hand {
+                hand_type: Type::HighCard,
+                bid: bid,
+                cards: cards,
+                str: str.to_owned()
             })
         },
         4 => {
-            Some(Entry {
-                hand_type: Type::OnePair(hand),
-                bid: bid
+            Some(Hand {
+                hand_type: Type::OnePair,
+                bid: bid,
+                cards: cards,
+                str: str.to_owned()
             })
         },
         3 => {
             if let Some(_) = hm.keys().into_iter().filter(|key| hm[key] == 3).collect::<Vec<_>>().get(0) {
-                Some(Entry {
-                    hand_type: Type::ThreeOfAKind(hand),
-                    bid: bid
+                Some(Hand {
+                    hand_type: Type::ThreeOfAKind,
+                    bid: bid,
+                    cards: cards,
+                    str: str.to_owned()
                 })
             }
             else {
-                Some(Entry {
-                    hand_type: Type::TwoPair(hand),
-                    bid: bid
+                Some(Hand {
+                    hand_type: Type::TwoPair,
+                    bid: bid,
+                    cards: cards,
+                    str: str.to_owned()
                 })
             }
         },
         2 => {
             if let Some(_) = hm.keys().into_iter().filter(|key| hm[key] == 3).collect::<Vec<_>>().get(0) {
-                Some(Entry {
-                    hand_type: Type::FullHouse(hand),
-                    bid: bid
+                Some(Hand {
+                    hand_type: Type::FullHouse,
+                    bid: bid,
+                    cards: cards,
+                    str: str.to_owned()
                 })
             }
             else {
-                Some(Entry {
-                    hand_type: Type::FourOfAKind(hand),
-                    bid: bid
+                Some(Hand {
+                    hand_type: Type::FourOfAKind,
+                    bid: bid,
+                    cards: cards,
+                    str: str.to_owned()
                 })
             }
 
         },
         1 => {
-            Some(Entry {
-                hand_type: Type::FiveOfAKind(hand),
-                bid: bid
+            Some(Hand {
+                hand_type: Type::FiveOfAKind,
+                bid: bid,
+                cards: cards,
+                str: str.to_owned()
             })
         }
         _ => None
